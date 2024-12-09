880377ef23a252658e694c4cde0bd76d2fa6cf02
Dharmveer Singh <singhdharmu89@gmail.com>
2024-12-09 16:26:16 +0530

    create book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -2,10 +2,16 @@ import { NextFunction, Request, Response } from "express";
 import cloudinary from "../config/cloudinary";
 import { IBook } from "./types/book";
 import path from "node:path";
+import fs from "node:fs";
 import { catchError } from "../utils/basicError";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
 
-const createBook = async (req: Request, res: Response, next: NextFunction) => {
+const createBook = async (
+  req: Request<object, object, IBook>,
+  res: Response,
+  next: NextFunction
+) => {
   const files = req.files as { [fieldname: string]: Express.Multer.File[] };
   const coverImageMineType = files.coverImage[0].mimetype.split("/").at(-1);
   const fileName = files.coverImage[0].filename;
@@ -28,22 +34,53 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     "../../public/data/uploads",
     bookFileName
   );
-  const [_error, _uploadResult] = await catchError(uploadResult());
 
+  const bookFileUploadResult = async () =>
+    await cloudinary.uploader.upload(booKFilePath, {
+      resource_type: "raw",
+      filename_override: bookFileName,
+      folder: "books",
+      format: "pdf",
+    });
+
+  const [_error, _uploadCoverResult] = await catchError(uploadResult());
   if (_error) {
     return next(createHttpError(500, `error while uploading ${_error}`));
   }
 
-  const bookFileUploadResult = await cloudinary.uploader.upload(booKFilePath, {
-    resource_type: "raw",
-    filename_override: bookFileName,
-    folder: "books",
-    format: "pdf",
+  const [_errorPdf, _uploadPdfResult] = await catchError(
+    bookFileUploadResult()
+  );
+  if (_errorPdf) {
+    return next(createHttpError(500, `error while uploading pdf ${_errorPdf}`));
+  }
+
+  const newBook = await bookModel.create({
+    title: req.body.title,
+    author: "671d2ca2175008467148571f",
+    coverImage: _uploadCoverResult?.secure_url,
+    description: req.body.description,
+    genre: req.body.genre,
+    file: _uploadPdfResult?.secure_url,
   });
 
-  console.log("this_is_upload_result", bookFileUploadResult);
+  // delete temp file
+  const [coverUnlinkError] = await catchError(fs.promises.unlink(filePath));
 
-  res.json({ message: "Create Book" });
+  if (coverUnlinkError) {
+    return next(
+      createHttpError(500, `error while unlinking cover ${coverUnlinkError}`)
+    );
+  }
+  const [pdfUnlinkError] = await catchError(fs.promises.unlink(booKFilePath));
+
+  if (pdfUnlinkError) {
+    return next(
+      createHttpError(500, `error while unlinking pdf ${pdfUnlinkError}`)
+    );
+  }
+
+  res.status(201).json({ newBook, id: newBook._id });
 };
 
 export { createBook };
